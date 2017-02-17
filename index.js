0d25d1623f053c808f567d9c92f882abfd288bc5
Dimitri del Marmol <dimitri.delmarmol@gmail.com>
2017-02-17 11:31:08 +0100

    Cleanup

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+
+/*jshint esversion: 6 */
 "use strict";
 
 var http = require("http");
@@ -9,11 +11,55 @@ var RED = require("node-red");
 
 var packageJson = require("./package.json");
 
-// See default settings.js file for other options
+
 var settings = {
-    // the tcp port that the Node-RED web server is listening on
+
+    userDir: path.resolve(__dirname, "data"),
+    nodesDir: path.resolve(__dirname, "data/nodes"),
+
     uiPort: 1880,
     uiHost: "0.0.0.0",
+
+    httpStatic: "public",
+    httpAdminRoot: "/edit/",
+    httpNodeRoot: "/endpoints/",
+    httpNodeCors: {
+        origin: "*",
+        methods: "GET,PUT,PATCH,POST,DELETE"
+    },
+    swagger: {
+      template: {
+        swagger: "2.0",
+        info: {
+          title: packageJson.name,
+          description: '[HTTP/1.1](https://tools.ietf.org/html/rfc2616)',
+          version: packageJson.version
+        }
+      }
+    },
+
+    // Anything in this hash is globally available to all functions.
+    // It is accessed as context.global.
+    // eg:
+    //    functionGlobalContext: { os:require('os') }
+    // can be accessed in a function block as:
+    //    context.global.os
+    functionGlobalContext: {
+        crypto: require('crypto'),
+        _: require('underscore'),
+        async: require('async')
+    },
+
+    httpNodeMiddleware: (req,res,next) => {
+      res.set("x-powered-by", "radapi");
+      next();
+    },
+    // If you need to set an http proxy please set an environment variable
+    // called http_proxy (or HTTP_PROXY) outside of Node-RED in the operating system.
+    // For example - http_proxy=http://myproxy.com:8080
+    // (Setting it here will have no effect)
+    // You may also specify no_proxy (or NO_PROXY) to supply a comma separated
+    // list of domains to not proxy, eg - no_proxy=.acme.co,.acme.co.uk
 
     // Retry time in milliseconds for MQTT connections
     mqttReconnectTime: 15000,
@@ -36,125 +82,11 @@ var settings = {
     // Enabled pretty-printing of the flow within the flow file
     flowFilePretty: true,
 
-    // By default, all user data is stored in the Node-RED install directory. To
-    // use a different location, the following property can be used
-    userDir: path.resolve(__dirname, "data"),
-
-    // Node-RED scans the `nodes` directory in the install directory to find nodes.
-    // The following property can be used to specify an additional directory to scan.
-    nodesDir: path.resolve(__dirname, "data/nodes"),
-
-    // By default, the Node-RED UI is available at http://localhost:1880/
-    // The following property can be used to specifiy a different root path.
-    // If set to false, this is disabled.
-    httpAdminRoot: "/edit/",
-
-    // You can protect the user interface with a userid and password by using the following property.
-    // The password must be an md5 hash  eg.. 5f4dcc3b5aa765d61d8327deb882cf99 ('password')
-    //httpAdminAuth: {user:"user",pass:"5f4dcc3b5aa765d61d8327deb882cf99"},
-
-    // Some nodes, such as HTTP In, can be used to listen for incoming http requests.
-    // By default, these are served relative to '/'. The following property
-    // can be used to specifiy a different root path. If set to false, this is
-    // disabled.
-    httpNodeRoot: "/endpoints/",
-
-    // To password protect the node-defined HTTP endpoints, the following property
-    // can be used.
-    // The password must be an md5 hash  eg.. 5f4dcc3b5aa765d61d8327deb882cf99 ('password')
-    //httpNodeAuth: {user:"user",pass:"5f4dcc3b5aa765d61d8327deb882cf99"},
-
-    // When httpAdminRoot is used to move the UI to a different root path, the
-    // following property can be used to identify a directory of static content
-    // that should be served at http://localhost:1880/.
-    httpStatic: "public",
-
-    // To password protect the static content, the following property can be used.
-    // The password must be an md5 hash  eg.. 5f4dcc3b5aa765d61d8327deb882cf99 ('password')
-    //httpStaticAuth: {user:"user",pass:"5f4dcc3b5aa765d61d8327deb882cf99"},
-
-    // The following property can be used in place of 'httpAdminRoot' and 'httpNodeRoot',
-    // to apply the same root to both parts.
-    //httpRoot: '/red',
-
-    // The following property can be used in place of 'httpAdminAuth' and 'httpNodeAuth',
-    // to apply the same authentication to both parts.
-    //httpAuth: {user:"user",pass:"5f4dcc3b5aa765d61d8327deb882cf99"},
-
-    // The following property can be used to disable the editor. The admin API
-    // is not affected by this option. To disable both the editor and the admin
-    // API, use either the httpRoot or httpAdminRoot properties
-    disableEditor: false,
-
-    // The following property can be used to enable HTTPS
-    // See http://nodejs.org/api/https.html#https_https_createserver_options_requestlistener
-    // for details on its contents.
-    // See the comment at the top of this file on how to load the `fs` module used by
-    // this setting.
-    //
-    //https: {
-    //    key: fs.readFileSync('privatekey.pem'),
-    //    cert: fs.readFileSync('certificate.pem')
-    //},
-
-    // The following property can be used to configure cross-origin resource sharing
-    // in the HTTP nodes.
-    // See https://github.com/troygoode/node-cors#configuration-options for
-    // details on its contents. The following is a basic permissive set of options:
-    httpNodeCors: {
-        origin: "*",
-        methods: "GET,PUT,PATCH,POST,DELETE"
-    },
-
-    // If you need to set an http proxy please set an environment variable
-    // called http_proxy (or HTTP_PROXY) outside of Node-RED in the operating system.
-    // For example - http_proxy=http://myproxy.com:8080
-    // (Setting it here will have no effect)
-    // You may also specify no_proxy (or NO_PROXY) to supply a comma separated
-    // list of domains to not proxy, eg - no_proxy=.acme.co,.acme.co.uk
-
-    // The following property can be used to add a custom middleware function
-    // in front of all http in nodes. This allows custom authentication to be
-    // applied to all http in nodes, or any other sort of common request processing.
-    //httpNodeMiddleware: function(req,res,next) {
-    //   // Handle/reject the request, or pass it on to the http in node
-    //   // by calling next();
-    //   next();
-    //},
-
-    // Anything in this hash is globally available to all functions.
-    // It is accessed as context.global.
-    // eg:
-    //    functionGlobalContext: { os:require('os') }
-    // can be accessed in a function block as:
-    //    context.global.os
-
-    functionGlobalContext: {
-        // os:require('os'),
-        // bonescript:require('bonescript'),
-        // jfive:require('johnny-five'),
-        // j5board:require('johnny-five').Board({repl:false})
-        crypto: require('crypto'),
-        _: require('underscore'),
-        async: require('async')
-    },
-
     // The following property can be used to order the categories in the editor
     // palette. If a node's category is not in the list, the category will get
     // added to the end of the palette.
     // If not set, the following default order is used:
     paletteCategories: ["subflows", "input", "output", "function", "storage",  "advanced", "social", "analysis"],
-
-    swagger: {
-      template: {
-        swagger: "2.0",
-        info: {
-          title: packageJson.name,
-          description: '[Edit](/edit/ target="_blank"){:target="_blank"} | [HTTP](https://tools.ietf.org/html/rfc2616)',
-          version: packageJson.version
-        }
-      }
-    },
 
     // Configure the logging output
     logging: {
@@ -176,7 +108,6 @@ var settings = {
 };
 
 var app = express();
-app.disable("x-powered-by");
 app.set("etag", "strong");
 app.use("/", express.static(path.resolve(__dirname, "public")));
 

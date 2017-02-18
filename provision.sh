be69b03a23058d9e5f295e63953c37f8bfd0498a
Dimitri del Marmol <dimitri.delmarmol@gmail.com>
2017-02-18 17:30:45 +0100

    Bugfix

--- provision.sh
+++ provision.sh
@@ -3,6 +3,6 @@
 git clone --depth 1 https://github.com/ddm/radapi /opt/radapi
 cd /opt/radapi
 npm install
-mkdir -p /opt/node/data/
+mkdir -p /opt/radapi/data/
 adduser radapi
 chown -R radapi:radapi /opt/radapi/
